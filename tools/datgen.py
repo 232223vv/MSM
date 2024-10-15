6dda1592486d7b64126f0f1af1b7692e6af847ab
23 <15016494521@163.com>
2024-10-15 13:12:32 +0800

    new tool

--- tools/datgen.py
+++ tools/datgen.py
@@ -9,9 +9,11 @@ squ_duty = [0.3, 0.5, 0.7, 0.9]
 sin_cnt = np.linspace(0, 2*np.pi, numOFsample)
 sin_data = (128 * np.sin(sin_cnt) + 128)
 sin_data = sin_data.astype(int)
+print(f"{len(sin_data)}\n")
 
 s2_cnt = [i for i in range(numOFsample)]
 saw_data = [int(i * 255 / (numOFsample - 1)) for i in s2_cnt]
+print(f"{len(saw_data)}\n")
 # squ30_data = []
 # squ50_data = []
 # squ70_data = []
@@ -26,6 +28,7 @@ saw_data = [int(i * 255 / (numOFsample - 1)) for i in s2_cnt]
 
 tri_cnt = [numOFsample - i if i >= (numOFsample / 2) else i for i in range(numOFsample)]
 tri_data = [int(i * 255 / (numOFsample / 2)) for i in tri_cnt]
+print(f"{len(tri_data)}\n")
 # print(f"{tri_data[1000]} / {tri_data[1999]}\n")
 # plt.plot(s2_cnt, tri_data)
 # plt.show()
@@ -34,20 +37,21 @@ for wave in dataFile:
     f = open(f"D:\\GITProjects\\MSM\\src\\SIGNAL_GENERATER\\{wave}_{numOFsample}hex.dat", "w")
     if(wave == "sin"):
         for item in sin_data:
+            print(item)
             data = str(hex(item)).split("x")[1]
             if len(data) == 1:
                 data = "0" + data
-                f.writelines(f"{data}\n")
+            f.write(f"{data}\n")
     elif(wave == "tri"):
         for item in tri_data:
             data = str(hex(item)).split("x")[1]
             if len(data) == 1:
                 data = "0" + data
-                f.writelines(f"{data}\n")
+            f.write(f"{data}\n")
     elif(wave == "saw"):
         for item in saw_data:
             data = str(hex(item)).split("x")[1]
             if len(data) == 1:
                 data = "0" + data
-                f.writelines(f"{data}\n")
+            f.write(f"{data}\n")
     f.close()
